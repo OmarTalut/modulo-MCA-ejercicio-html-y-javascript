bbae41e9db7cea6584775d6959ee3d97538e1530
Omar Morales <omartalut@outlook.com>
2025-10-16 12:11:10 -0600

    se envia el trabajo de la clase 2 html y javascript

---
+++ ejerciciosLogicaJavascript.js
@@ -0,0 +1,18 @@
+let numeros= [10, 25, 3, 99, 42];
+let mayor= numeros [0]
+for (let i= 0; i < numeros.length; i++){
+    if (numeros[i]> mayor){
+        mayor=numeros[i];
+    }
+}
+console.log ( mayor);
+
+let valores= [10,55,73,42,91,18];
+let mayor50= 0;
+for (let i=0; i< valores.length; i++){
+    if (valores[i]> 50){
+        mayor50++;
+    }
+}
+console.log (mayor50)
+
